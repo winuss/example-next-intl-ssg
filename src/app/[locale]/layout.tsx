09ad1fb1c54777d051c66fed9a1f63a0976c0c9c
유성수/Seongsu yoo(HIM_웹팀) <winuss@hybecorp.com>
2024-11-20 10:22:58 +0900

    ssr

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,36 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  // Ensure that the incoming `locale` is valid
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
+
+// export function generateStaticParams() {
+//   return routing.locales.map((locale) => ({ locale }));
+// }
